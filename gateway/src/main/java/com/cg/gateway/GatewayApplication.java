abdfe3ae1f3c5076907cd592a3629c42193bb123
Anurag <anurag-kumar.yadav@capgemini.com>
2024-12-17 17:54:35 +0530

    Allowed Cors for different routes

--- gateway/src/main/java/com/cg/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cg/gateway/GatewayApplication.java
@@ -1,12 +1,43 @@
 package com.cg.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+
+
+//----------------
+
+
+import java.util.List;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
+	
+	@Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:5173")); // Specific origin
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
